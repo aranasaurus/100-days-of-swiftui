81f309288f74ea31da05d4ee36cab202eb024e64
Ryan Arana <ryan.arana@gmail.com>
2019-10-26 16:07:09 -0700

    Get adding new words via TextField and displaying them working

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -9,8 +9,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+                    .autocapitalization(.none)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+
+                List(usedWords, id: \.self) {
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
+                }
+            }
+        }.navigationBarTitle(rootWord)
+    }
+
+    private func addNewWord() {
+        let word = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+
+        guard word.count > 0 else { return }
+
+        // TODO: moar validation
+
+        usedWords.insert(word, at: 0)
+        newWord = ""
     }
 }
 
