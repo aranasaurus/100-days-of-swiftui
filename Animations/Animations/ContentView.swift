38a40ddad94c7ae684e1c307d4212030334f7be7
Ryan Arana <ryan.arana@gmail.com>
2019-10-27 10:29:53 -0700

    Add Animations project

---
+++ Animations/Animations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Ryan Arana on 10/27/19.
+//  Copyright © 2019 aranasaurus.com. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
