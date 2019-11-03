44d3d1bd4c521c304169db7be8b6060a1334ec68
Ryan Arana <ryan.arana@gmail.com>
2019-11-02 18:42:42 -0800

    Add project for Multiplication Tables challenge

---
+++ MultiplicationTables/MultiplicationTables/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MultiplicationTables
+//
+//  Created by Ryan Arana on 11/2/19.
+//  Copyright © 2019 aranasaurus.com. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
